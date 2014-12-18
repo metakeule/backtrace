3fdeff4a7911f10837e802e803cf6c2616d90361
Marc Rene Arns <arns@stridor.net>
2014-12-18 15:44:22 +0100

    make test independent from pkg path

--- backtrace_test.go
+++ backtrace_test.go
@@ -14,13 +14,20 @@ func TestBackTrace(t *testing.T) {
 		return true
 	})
 
-	if len(trace) != 2 {
-		t.Errorf("len of trace should be 2, but is: %d", len(trace))
+	if len(trace) != 3 {
+		t.Errorf("len of trace should be 3, but is: %d", len(trace))
 	}
 
-	traceFunction0 := "github.com/metakeule/backtrace.TestBackTrace"
-	if trace[0].Function != traceFunction0 {
-		t.Errorf("trace[0].Function should be %#v but is %#v", traceFunction0, trace[0].Function)
+	traceFunction0 := ".TestBackTrace"
+
+	idx := strings.LastIndex(trace[0].Function, ".")
+
+	if idx == -1 {
+		t.Errorf("trace[0].Function must have .but is %#v", trace[0].Function)
+	}
+
+	if trace[0].Function[idx:] != traceFunction0 {
+		t.Errorf("trace[0].Function[idx:] should be %#v but is %#v", traceFunction0, trace[0].Function[idx:])
 	}
 
 	if trace[0].Line != 10 {
